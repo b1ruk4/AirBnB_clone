f7db9525572469f237662baa254cd240ee35a1d4
b1ruk4 <birukwondimubkk@gmail.com>
2022-09-01 10:51:37 -0700

    docstring

--- models/user.py
+++ models/user.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""defines class User"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """defining User attributes"""
     email = ""
     password = ""
     first_name = ""
