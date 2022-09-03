2bf6da02c92bf419865c545ac8c4637c435d6b70
Nellykhris <ifeomaomeonu@gmail.com>
2022-09-03 22:44:42 +0100

    State model

---
+++ tests/test_models/test_state_model.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""unit testing the state model module"""
+import models.state
+from models import storage
+import unittest
+
+
+class TestStateModel(unittest.TestCase):
+    """The TestStateModel class for testing the state_model"""
+
+    def setUp(self):
+        """creating a new object for the class"""
+        self.new = models.state.State()
+
+    def test_init(self):
+        """testing the init function"""
+        self.assertIsInstance(self.new, models.state.State)
+        self.assertTrue(self.new.id)
+        self.assertTrue(self.new.created_at)
+        self.assertTrue(self.new.updated_at)
+
+    def test_storage(self):
+        """checking the storage"""
+        obj = storage.all()
+        key = str(self.new.__class__.__name__) + "." + str(self.new.id)
+        self.assertIn(key, obj)
+
+    def test_dict(self):
+        """test for checking about the to_dict method"""
+        _dict = self.new.to_dict()
+        self.assertTrue(_dict)
+        self.new.name = "My state Model"
+        self.assertIn("name", self.new.__dict__)
+
+    def test_save(self):
+        """test for checking if the instance got saved"""
+        self.new.name = "My state Model"
+        self.assertFalse(self.new.save())
+
+    if __name__ == '__main__':
+        unittest.main()
