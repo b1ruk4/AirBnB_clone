897e2c0f46f595327549248c68814a36d56e13e6
b1ruk4 <birukwondimubkk@gmail.com>
2022-09-05 12:03:47 -0700

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -55,6 +55,6 @@ class BaseModel:
         """
         new_dict = self.__dict__
         new_dict['__class__'] = self.__class__.__name__
-        new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
