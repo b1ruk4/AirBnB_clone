7ca72daa90f577cf3dfad7802f8e31e62b1bbede
b1ruk4 <birukwondimubkk@gmail.com>
2022-09-05 16:03:18 -0700

    fix time

--- models/base_model.py
+++ models/base_model.py
@@ -19,8 +19,8 @@ class BaseModel:
         public instance attributes initialization
         """
         self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
         if kwargs:
             for key, value in kwargs.items():
@@ -46,7 +46,7 @@ class BaseModel:
             updates the public instance attribute updated_at
             with the current datetime
         """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
